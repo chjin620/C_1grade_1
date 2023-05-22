bb1b3a7fed78dae2554502ac6686fa1d206d5420
airbsb <126002395+chjin620@users.noreply.github.com>
2023-05-22 16:39:55 +0900

    주석 추가

--- chapter_03/code_3_13.c
+++ chapter_03/code_3_13.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
 int main()
-{
-	char c1=128;
-	char c2=-129;
+{   
+	//char 타입의 표현 범위== -128~127
+	char c1=128; //최댓값+1
+	char c2=-129; //최솟값 -1
 	printf("c1 : %d\nc2 : %d\n",c1,c2);
+	//결과=> c1 : -128\n c2 : 127
 	
-	unsigned char uc1=256;
-	unsigned char uc2=-1;
+	//unsigned char 타입의 표현범위==0~255
+	unsigned char uc1=256; //최댓값 +1
+	unsigned char uc2=-1; //최솟값 -1
 	printf("uc1 : %d\nuc2 : %d\n",uc1,uc2);
+	//결과=> uc1 : 0\n uc2 : 255
 }
